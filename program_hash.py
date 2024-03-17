f2d52efbc206806f7b540563dfd9037b0433068e
JaFi01 <filipek.jaroslaw1@gmail.com>
2024-03-17 23:49:20 +0100

    modified file checking hash of file

--- program_hash.py
+++ program_hash.py
@@ -1,19 +1,18 @@
-#This is task, to check if linux .iso file which I'be downloaded has correct hash
-#Path implied here is specific to me, as I run this on my own mahine via WSL
-
 import hashlib
+#This is task, to check if linux .iso file which I'be downloaded has correct hash
+#Path implied in this script is specific to me, as I run this on my own mahine via WSL
 
 CORRECT_HASH = 'b284afcc298cc6f5da6ab4d483318c453b2074485974b71b16fdfc7256527cb1' # *linuxmint-21.3-xfce-64bit.iso from https://ftp.heanet.ie/mirrors/linuxmint.com/stable/21.3/sha256sum.txt
 
 
 def calculate_file_hash(file_path):
-    """Funkction calculating hash SHA-256 for choosen file.
+    """Calculate the SHA-256 hash for a given binary file.
 
     Args:
-        file_path (str): Path to file.
+        file_path (str): Path to the file.
 
     Returns:
-        str: Hash SHA-256 for file.
+        str: SHA-256 hash for the File.
     """
     sha256_hash = hashlib.sha256()
     with open(file_path, "rb") as f:
@@ -21,14 +20,23 @@ def calculate_file_hash(file_path):
             sha256_hash.update(byte_block)
     return sha256_hash.hexdigest()
 
+
 def main():
     path_to_file = "/mnt/c/Users/jaros/Downloads/linuxmint-21.3-xfce-64bit.iso"
-    file_hash = calculate_file_hash(path_to_file)
-
-    if file_hash == CORRECT_HASH:
-        print("Hash is correct!")
+    choice = input("Enter 'h' to check if hash is correct or 'r' to read the hash of the file: ").strip().lower()
+    if choice == 'h':
+        file_hash = calculate_file_hash(path_to_file)
+        if file_hash == CORRECT_HASH:
+            print("Hash is correct!")
+        else:
+            print("Hash is incorrect.")
+        #print("File hash:", file_hash)
+    elif choice == 'r':
+        file_hash = calculate_file_hash(path_to_file)
+        print("Hash of the file:", file_hash)
     else:
-        print("Hash is incorrect.")
+        print("Invalid choice.")
+
 
 if __name__ == "__main__":
     main()
